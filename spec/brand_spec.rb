8cf69fc6a979289b6505fed1e5ff213581e420c3
John Michael <john.parsons85210@gmail.com>
2017-09-29 19:15:21 -0700

    add brand_spec

--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -1,5 +1,6 @@
-# require('spec_helper')
-#
-# describe(Brand) do
-#   it {should have_and_belong_to_many(:shoes)}
-# end
+require('spec_helper')
+
+describe(Brand) do
+  it {should have_many(:inventories)}
+  it {should have_many(:stores)}
+end
